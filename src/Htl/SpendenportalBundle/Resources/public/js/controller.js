b8e41fe1c9435fecc1ebad5ff2dc9acc8b8038fa
riccoDeveloper <ricco1skombar@gmail.com>
2017-01-24 17:18:11 +0100

    entdecken seite on fleek außer suche

--- src/Htl/SpendenportalBundle/Resources/public/js/controller.js
+++ src/Htl/SpendenportalBundle/Resources/public/js/controller.js
@@ -1,7 +1,158 @@
 var app = angular.module('SpendenverbindetApp', []);
 
-app.controller('spendenverbindetController', function($scope) {
+app.config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('[[').endSymbol(']]');
+});
 
-    $scope.testvariable = "Hello World from Angular";
+app.controller('spendenverbindetController', function($scope, $http) {
+
+    $scope.showLoadingIndicator = false;
+    $scope.activeCategorie = "Alle Projekte";
+    $scope.allCategories = [];
+
+    // Get all categories in an array
+    $http({
+        method: 'GET',
+        url: '/categories'
+    }).then(function successCallback(response) {
+
+        $scope.allCategories = response.data;
+
+    }, function errorCallback(response) {
+
+    });
+
+
+    $scope.projects = [];
+    $scope.projectsPrepared = [];
+
+
+    // Get all projects of all categories in an array
+    $scope.showLoadingIndicator = true;
+
+    $http({
+        method: 'GET',
+        url: '/projects'
+    }).then(function successCallback(response) {
+
+        $scope.showLoadingIndicator = false;
+        $scope.projects = [];
+        $scope.projectsPrepared = [];
+
+
+        for(var u=0;u<Object.keys(response.data).length;u++){
+            $scope.projects.push(response.data[u]);
+        }
+
+        if( $scope.projects.length > 3){
+
+                var itarationValue = ($scope.projects.length / 3) | 0;
+
+                for(var z=0;z<itarationValue;z++){
+                    $scope.projectsPrepared.push([$scope.projects[0],$scope.projects[1],$scope.projects[2]]);
+                    $scope.projects.splice(0, 3);
+                }
+
+                if($scope.projects.length % 3 != 0){
+                    $scope.projectsPrepared.push($scope.projects);
+                }
+
+        }else{
+            $scope.projectsPrepared.push($scope.projects);
+        }
+
+
+        console.log($scope.projectsPrepared);
+
+    }, function errorCallback(response) {
+
+    });
+
+
+    // Get all projects to the related categorie id
+    $scope.loadProjectsWithId = function(id, categoryName){
+
+        $scope.showLoadingIndicator = true;
+        $scope.projects = [];
+        $scope.projectsPrepared = [];
+
+        $scope.activeCategorie = categoryName;
+
+        $http({
+            method: 'GET',
+            url: '/projects/'+id
+        }).then(function successCallback(response) {
+
+            $scope.showLoadingIndicator = false;
+
+            for(var u=0;u<Object.keys(response.data).length;u++){
+                $scope.projects.push(response.data[u]);
+            }
+
+            if( $scope.projects.length > 3){
+
+                var itarationValue = ($scope.projects.length / 3) | 0;
+
+                for(var z=0;z<itarationValue;z++){
+                    $scope.projectsPrepared.push([$scope.projects[0],$scope.projects[1],$scope.projects[2]]);
+                    $scope.projects.splice(0, 3);
+                }
+
+                if($scope.projects.length % 3 != 0){
+                    $scope.projectsPrepared.push($scope.projects);
+                }
+
+            }else{
+                $scope.projectsPrepared.push($scope.projects);
+            }
+
+        }, function errorCallback(response) {
+
+        });
+    }
+
+    
+
+    // Get all projects of all categories in an array
+    $scope.loadAllProjects = function(){
+
+        $scope.showLoadingIndicator = true;
+        $scope.projects = [];
+        $scope.projectsPrepared = [];
+
+        $scope.activeCategorie = "Alle Projekte";
+
+        $http({
+            method: 'GET',
+            url: '/projects'
+        }).then(function successCallback(response) {
+
+            $scope.showLoadingIndicator = false;
+
+            for(var u=0;u<Object.keys(response.data).length;u++){
+                $scope.projects.push(response.data[u]);
+            }
+
+            if( $scope.projects.length > 3){
+
+                var itarationValue = ($scope.projects.length / 3) | 0;
+
+                for(var z=0;z<itarationValue;z++){
+                    $scope.projectsPrepared.push([$scope.projects[0],$scope.projects[1],$scope.projects[2]]);
+                    $scope.projects.splice(0, 3);
+                }
+
+                if($scope.projects.length % 3 != 0){
+                    $scope.projectsPrepared.push($scope.projects);
+                }
+
+            }else{
+                $scope.projectsPrepared.push($scope.projects);
+            }
+
+        }, function errorCallback(response) {
+
+        });
+    }
 
 });
