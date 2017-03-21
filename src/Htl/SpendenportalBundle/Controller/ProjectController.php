2d1261e46377fbe52d6a7b920413fee128762ba2
fertlthomas <thomas18fertl@gmail.com>
2017-03-21 15:58:20 +0100

    update filename unique making method

--- src/Htl/SpendenportalBundle/Controller/ProjectController.php
+++ src/Htl/SpendenportalBundle/Controller/ProjectController.php
@@ -603,7 +603,7 @@ class ProjectController extends Controller
                                     return new JsonResponse("Sorry, your file is too large. Maximal 750kB");
                                 }
                                 // Allow certain file formats
-
+                                /*
                                 if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
                                     && $imageFileType != "gif" && $imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG"
                                     && $imageFileType != "GIF" && $imageFileType != ""
@@ -611,6 +611,7 @@ class ProjectController extends Controller
                                     $uploadOk = 0;
                                     return new JsonResponse("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
                                 }
+                                */
                                 // Check if $uploadOk is set to 0 by an error
                                 if ($uploadOk == 0) {
                                     return new JsonResponse("Sorry, your file was not uploaded.");
